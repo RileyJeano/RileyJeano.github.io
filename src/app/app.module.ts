a5dc391e74c241c2ccffb85d2edc6124e27a5826
Riley <rileyjeanpatrick@gmail.com>
2022-02-15 12:36:00 -0500

    art page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArtComponent } from './art/art.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtComponent
   ],
   imports: [
     BrowserModule,
